9e0a9fc4b7327c2f9417142cc9a86abb822fcf54
TuckerRasbury <70918983+TuckerRasbury@users.noreply.github.com>
2020-11-30 00:05:23 -0800

    Adding code as of 7.3.6 skill drills

--- queries.sql
+++ queries.sql
@@ -115,3 +115,176 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
 
+-- 7.3.2 Joining Tables in theory!
+-- Tip: https://www.techonthenet.com/postgresql/joins.php
+
+-- first clear retirement_info
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- 7.3.3 Joins in Action
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Use Aliases for Code Readability
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Using abbreviations
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Note: Joins aren't always completed instantly; the performance can vary based on different factors. For example, joining more than three tables will be slower than joining two tables. The size of the datasets being joined is another factor to consider. Even the physical design of the table can play a part!
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Testing that abbreviated code works: 
+-- SELECT * FROM current_emp
+
+-- 7.3.4 Use COUNT, GROUP BY, and ORDER BY
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number, ordered by dept_no
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO skill_drill
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM skill_drill
+
+-- 7.3.5 Create Additional Lists
+-- 1. Employee Information: A list of employees containing their unique employee number, their last name, first name, 
+--		gender, and salary
+-- 2. Management: A list of managers for each department, including the department number, name, and the manager's 
+-- 		employee number, last name, first name, and the starting and ending employment dates
+-- 3. Department Retirees: An updated current_emp list that includes everything it currently has, but also the 
+-- 		employee's departments
+
+-- List #1 - The first requested list is general employee information, but with their current salaries included.
+SELECT e.emp_no, 
+	e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+-- List #2 - This list includes the manager's employee number, first name, last name, and their starting and ending employment dates.
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List #3 - The final list needs only to have the departments added to the current_emp table.
+SELECT ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+		
+-- Current Questions
+-- What's going on with the salaries?
+-- Why are there only five active managers for nine departments?
+-- Why are some employees appearing twice?
+
+-- 7.3.6 Creating a Tailored List
+-- To this point we have: Think of it: We started with only six CSV files and no real database or data management system in place. Now, we have created a model of the database with an ERD, imported data, and to tie it all together, we have performed many queries to help PH future-proof the company.
+
+-- A. Sales' Request 
+	--1) Join w/ dept_emp (emp_no, dept_no) and departments (dept_no, dept_name), 
+	--2) filtered for sales only, 
+	--3) filtered for sales and development
+ 
+-- A1)
+SELECT  
+
+FROM retirement_info as ri
+
+
+-- B. For Sales and Dev.
+	-- Perform A3 but using an IN command in a WHERE function
+
+
+
